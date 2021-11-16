a10f4940b8bc3dd1de5e6cab0eef38248d4f0783
sfiers <simon.fiers@outlook.com>
2021-11-16 14:36:00 +0100

    43 HTTP Post Request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
+import PostForm from './components/PostForm';
 import PostList from './components/PostList';
 
 function App() {
   return (
     <div className="App">
-      <PostList />
+      <PostForm />
+      {/* <PostList /> */}
     </div>
   );
 }
