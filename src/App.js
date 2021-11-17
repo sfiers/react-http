492d51f016a77cc618b8554c7408791a56e3b546
sfiers <simon.fiers@outlook.com>
2021-11-17 13:40:26 +0100

    3 useState with previous state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
+import HookCounterTwo from './components/HookCounterTwo';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
 
 function App() {
   return (
     <div className="App">
-      <HookCounter />
+      <HookCounterTwo />
+      {/* <HookCounter /> */}
       {/* <ClassCounter /> */}
       {/* <PostForm /> */}
       {/* <PostList /> */}
