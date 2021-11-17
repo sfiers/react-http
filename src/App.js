9c4d6fadeb46226c66749203c9979be2ec22834d
sfiers <simon.fiers@outlook.com>
2021-11-17 15:48:48 +0100

    7 useEffect after render

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
 import ClassCounter from './components/ClassCounter';
+import ClassCounterOne from './components/ClassCounterOne';
 import HookCounter from './components/HookCounter';
 import HookCounterFour from './components/HookCounterFour';
+import HookCounterOne from './components/HookCounterOne';
 import HookCounterThree from './components/HookCounterThree';
 import HookCounterTwo from './components/HookCounterTwo';
 import PostForm from './components/PostForm';
@@ -11,7 +13,9 @@ import PostList from './components/PostList';
 function App() {
   return (
     <div className="App">
-      <HookCounterFour />
+      <HookCounterOne />
+      {/* <ClassCounterOne /> */}
+      {/* <HookCounterFour /> */}
       {/* <HookCounterThree /> */}
       {/* <HookCounterTwo /> */}
       {/* <HookCounter /> */}
