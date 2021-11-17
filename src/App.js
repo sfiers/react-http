52d33f82c71f92272d0e3d7cc10331b434160770
sfiers <simon.fiers@outlook.com>
2021-11-17 20:38:24 +0100

    9 Run effects only once

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import ClassCounterOne from './components/ClassCounterOne';
+import ClassMouse from './components/ClassMouse';
 import HookCounter from './components/HookCounter';
 import HookCounterFour from './components/HookCounterFour';
 import HookCounterOne from './components/HookCounterOne';
 import HookCounterThree from './components/HookCounterThree';
 import HookCounterTwo from './components/HookCounterTwo';
+import HookMouse from './components/HookMouse';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterOne />
+      <HookMouse />
+      {/* <ClassMouse /> */}
+      {/* <HookCounterOne /> */}
       {/* <ClassCounterOne /> */}
       {/* <HookCounterFour /> */}
       {/* <HookCounterThree /> */}
